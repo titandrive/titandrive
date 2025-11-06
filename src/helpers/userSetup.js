cb55689a50b06b149c00c1f19de574ecbbffdb22
Jimothy <240087026+titandrive@users.noreply.github.com>
2025-11-05 16:27:02 -0800

    Update userSetup.js

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -4,22 +4,8 @@ function userMarkdownSetup(md) {
 
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the config instantiated in /.eleventy.js.
-
-  // --- ✅ Added Zoombox section ---
-  eleventyConfig.addPassthroughCopy({ "src/user": "user" });
-
-  eleventyConfig.addTransform("inject-zoombox", function (content, outputPath) {
-    if (outputPath && outputPath.endsWith(".html")) {
-      const headTag = `<link rel="stylesheet" href="/user/zoombox.css">`;
-      const bodyEndTag = `<script src="/user/zoombox.js"></script>`;
-
-      let out = content.replace("</head>", `${headTag}\n</head>`);
-      out = out.replace("</body>", `${bodyEndTag}\n</body>`);
-      return out;
-    }
-    return content;
-  });
 }
 
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
+
